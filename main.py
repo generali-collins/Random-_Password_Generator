0239bc8bde92b2e4436f93f53799bd2201dbb015
generali-collins <wangacollins72@gmail.com>
2022-08-23 18:12:56 +0000

    Understanding the import modules through Random Password Generator Project

---
+++ main.py
@@ -0,0 +1,18 @@
+#Random Password Generator
+#Import the relevant modules
+import random
+import string
+print ('Hello, Welcome to Random Password Generator')
+#Password length
+length = int(input('\nEnter the length of the password: '))
+#Definitions
+upper = string.ascii_uppercase
+lower = string.ascii_lowercase
+numbers = string.digits
+symbols = string.punctuation
+#Combine data
+all = upper + lower + numbers + symbols
+#import random
+temp = random.sample(all,length)
+password = "".join(temp)
+print(password)
